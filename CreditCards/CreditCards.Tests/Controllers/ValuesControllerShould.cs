38be9d6f36a117f58e7d05a785553611fb093ec1
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-06 16:04:45 +1000

    Add sample api controller and provide testing for API controller

---
+++ CreditCards/CreditCards.Tests/Controllers/ValuesControllerShould.cs
@@ -0,0 +1,49 @@
+﻿using CreditCards.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace CreditCards.Tests.Controllers
+{
+    public class ValuesControllerShould
+    {
+        [Fact]
+        public void ReturnValues()
+        {
+            var sut = new ValuesController();
+
+            var result = sut.Get().ToArray();
+
+            Assert.Equal(2, result.Length);
+            Assert.Equal("value1", result[0]);
+            Assert.Equal("value2", result[1]);
+        }
+
+        [Fact]
+        public void ReturnBadRequest()
+        {
+            var sut = new ValuesController();
+
+            var result = sut.Get(0);
+
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+
+            Assert.Equal("Invalid request for id 0", badRequestResult.Value);
+        }
+
+        [Fact]
+        public void StartJobOk()
+        {
+            var sut = new ValuesController();
+
+            var result = sut.StartJob();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            Assert.Equal("Batch Job Started", okResult.Value);
+        }
+    }
+}
---
+++ CreditCards/CreditCards/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CreditCards.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            if (id < 1)
+                return BadRequest($"Invalid request for id {id}");
+
+            return Content($"Value {id}");
+        }
+
+        public IActionResult StartJob()
+        {
+            return Ok("Batch Job Started");
+        }
+    }
+}
