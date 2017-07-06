02645ebc76444006d58c4498f21a93ea5ef82543
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-06 14:55:58 +1000

    Add unit test for Index action of Apply Controller

---
+++ CreditCards/CreditCards.Tests/Controllers/ApplyControllerShould.cs
@@ -0,0 +1,29 @@
+﻿using CreditCards.Controllers;
+using CreditCards.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace CreditCards.Tests.Controllers
+{
+    public class ApplyControllerShould
+    {
+        private readonly Mock<ICreditCardApplicationRepository> _mockRepository;
+        // sut => System Under Test
+        private readonly ApplyController _sut;
+
+        public ApplyControllerShould()
+        {
+            _mockRepository = new Mock<ICreditCardApplicationRepository>();
+            _sut = new ApplyController(_mockRepository.Object);
+        }
+
+        [Fact]
+        public void ReturnViewForIndex()
+        {
+            var result = _sut.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
