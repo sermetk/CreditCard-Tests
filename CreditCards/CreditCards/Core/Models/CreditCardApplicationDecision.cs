be39950854395b5539861c935448113714f49687
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-06 13:36:07 +1000

    Add CreditCardApplicationEvaluator

---
+++ CreditCards/CreditCards/Core/Models/CreditCardApplicationDecision.cs
@@ -0,0 +1,10 @@
+﻿namespace CreditCards.Core.Models
+{
+    public enum CreditCardApplicationDecision
+    {
+        Unknown,
+        AutoAccepted,
+        AutoDeclined,
+        ReferredToHuman
+    }
+}
---
+++ CreditCards/CreditCards/Core/Models/CreditCardApplicationEvaluator.cs
@@ -0,0 +1,23 @@
+﻿namespace CreditCards.Core.Models
+{
+    public class CreditCardApplicationEvaluator
+    {
+        private const int _autoReferralMaxAge = 20;
+        private const int _highIncomeThreshhold = 100_000;
+        private const int _lowIncomeThreshhold = 20_000;
+
+        public CreditCardApplicationDecision Evaluate(CreditCardApplication application)
+        {
+            if (application.GrossAnnualIncome >= _highIncomeThreshhold)
+                return CreditCardApplicationDecision.AutoAccepted;
+
+            if (application.Age <= _autoReferralMaxAge)
+                return CreditCardApplicationDecision.ReferredToHuman;
+
+            if (application.GrossAnnualIncome < _lowIncomeThreshhold)
+                return CreditCardApplicationDecision.AutoDeclined;
+
+            return CreditCardApplicationDecision.ReferredToHuman;
+        }
+    }
+}
