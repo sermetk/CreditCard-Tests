a382f41838cf75a9f503cb267889ed52dbdc7576
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-07 15:46:47 +1000

    Provide anti forgery token in unit test

--- CreditCards/CreditCards.Integration.Test/CreditCardApplicationShould.cs
+++ CreditCards/CreditCards.Integration.Test/CreditCardApplicationShould.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -10,6 +15,9 @@ namespace CreditCards.Integration.Test
 {
     public class CreditCardApplicationShould
     {
+        private const string _antiForgeryFieldName = "__AFTField";
+        private const string _antiForgeryCookieName = "AFTCOokie";
+
         [Fact]
         public async Task RenderApplicationFormAsync()
         {
@@ -58,17 +66,32 @@ namespace CreditCards.Integration.Test
                         .AddEnvironmentVariables();
                 })
                 .UseEnvironment("Development")
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureServices(x => 
+                {
+                    x.AddAntiforgery(t => 
+                    {
+                        t.CookieName = _antiForgeryCookieName;
+                        t.FormFieldName = _antiForgeryFieldName;
+                    });
+                });
             //.UseApplicationInsights()
 
             var server = new TestServer(builder);
-
             var client = server.CreateClient();
+
+            var initialResponse = await client.GetAsync("/Apply");
+
+            var antiForgeryCookieValue = ExtractAntiForgeryCookieValueFrom(initialResponse);
+            var antiForgeryToken = ExtractAntiForgeryToken(await initialResponse.Content.ReadAsStringAsync());
 
             var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Apply");
 
+            postRequest.Headers.Add("Cookie", new CookieHeaderValue(_antiForgeryCookieName, antiForgeryCookieValue).ToString());
+
             var formData = new Dictionary<string, string>
             {
+                {_antiForgeryFieldName, antiForgeryToken},
                 {"FirstName", "Sarah"},
                 {"LastName", "Smith"},
                 {"Age", "18"},
@@ -84,6 +107,28 @@ namespace CreditCards.Integration.Test
             var responseString = await postReponse.Content.ReadAsStringAsync();
 
             Assert.Contains("Please provide a frequent flyer number", responseString);
+        }
+
+        private static string ExtractAntiForgeryCookieValueFrom(HttpResponseMessage response)
+        {
+            var antiForgeryCookie = response.Headers.GetValues("Set-Cookie").FirstOrDefault(x => x.Contains(_antiForgeryCookieName));
+
+            if (antiForgeryCookie is null)
+                throw new ArgumentException($"Cookie '{_antiForgeryCookieName}' not found in HTTP response", nameof(response));
+
+            var antiForgeryCookieValue = SetCookieHeaderValue.Parse(antiForgeryCookie).Value;
+
+            return antiForgeryCookieValue;
+        }
+
+        private string ExtractAntiForgeryToken(string htmlBody)
+        {
+            var requestVerificationTokenMatch = Regex.Match(htmlBody, $@"\<input name=""{_antiForgeryFieldName}"" type=""hidden"" value=""([^""]+)"" \/\>");
+
+            if (requestVerificationTokenMatch.Success)
+                return requestVerificationTokenMatch.Groups[1].Captures[0].Value;
+
+            throw new ArgumentException($"Anti forgery token '{_antiForgeryFieldName}' not found in HTML", nameof(htmlBody));
         }
     }
 }
