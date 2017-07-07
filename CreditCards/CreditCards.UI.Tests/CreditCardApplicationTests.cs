b717f8fec64bb8824f6238a4ef899a727b57a85c
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-07 17:38:00 +1000

    Add basic UI test by making sure the URL is working and has the correct title

--- CreditCards/CreditCards.UI.Tests/CreditCardApplicationTests.cs
+++ CreditCards/CreditCards.UI.Tests/CreditCardApplicationTests.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
 
 namespace CreditCards.UI.Tests
 {
-    public class CreditCardApplicationTests
+    public class CreditCardApplicationTests : IDisposable
     {
+        private readonly IWebDriver _driver;
+
+        public CreditCardApplicationTests()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+        [Fact]
+        public void ShouldLoadApplicationPage_SmokeTest()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:64559/apply");
+
+            Assert.Equal("Credit Card Application - CreditCards", _driver.Title);
+        }
     }
 }
