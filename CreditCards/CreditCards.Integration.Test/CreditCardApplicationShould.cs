d85dffe7ea084899c5d7b921dbd6696e010b5fcb
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-07 16:03:48 +1000

    Refactor CreditCardApplicationShould

--- CreditCards/CreditCards.Integration.Test/CreditCardApplicationShould.cs
+++ CreditCards/CreditCards.Integration.Test/CreditCardApplicationShould.cs
@@ -1,47 +1,24 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Net.Http.Headers;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace CreditCards.Integration.Test
 {
-    public class CreditCardApplicationShould
+    public class CreditCardApplicationShould : IClassFixture<TestServerFixture>
     {
-        private const string _antiForgeryFieldName = "__AFTField";
-        private const string _antiForgeryCookieName = "AFTCOokie";
+        private TestServerFixture _fixture;
+
+        public CreditCardApplicationShould(TestServerFixture fixture)
+        {
+            _fixture = fixture;
+        }
 
         [Fact]
         public async Task RenderApplicationFormAsync()
         {
-            // https://github.com/aspnet/Hosting/issues/959#issuecomment-286351703
-            // Fix problem with 500 errors when running the test server
-            var builder = new WebHostBuilder()
-                .UseContentRoot(@"C:\Users\MIhsan\Documents\GitHub\CreditCard-Tests\CreditCards\CreditCards")
-                .ConfigureAppConfiguration((hostingContext, config) =>  
-                {
-                    var env = hostingContext.HostingEnvironment;
-                    ((ConfigurationBuilder)config)
-                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
-                        .AddEnvironmentVariables();
-                })
-                .UseEnvironment("Development")
-                .UseStartup<Startup>();
-            //.UseApplicationInsights()
-
-            var server = new TestServer(builder);
-
-            var client = server.CreateClient();
-
-            var response = await client.GetAsync("/apply");
+            var response = await _fixture.Client.GetAsync("/apply");
 
             response.EnsureSuccessStatusCode();
 
@@ -53,45 +30,16 @@ namespace CreditCards.Integration.Test
         [Fact]
         public async Task NotAcceptPostedApplicationDetailsWithMissingFrequentFlyerNumber()
         {
-            // https://github.com/aspnet/Hosting/issues/959#issuecomment-286351703
-            // Fix problem with 500 errors when running the test server
-            var builder = new WebHostBuilder()
-                .UseContentRoot(@"C:\Users\MIhsan\Documents\GitHub\CreditCard-Tests\CreditCards\CreditCards")
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    var env = hostingContext.HostingEnvironment;
-                    ((ConfigurationBuilder)config)
-                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
-                        .AddEnvironmentVariables();
-                })
-                .UseEnvironment("Development")
-                .UseStartup<Startup>()
-                .ConfigureServices(x => 
-                {
-                    x.AddAntiforgery(t => 
-                    {
-                        t.CookieName = _antiForgeryCookieName;
-                        t.FormFieldName = _antiForgeryFieldName;
-                    });
-                });
-            //.UseApplicationInsights()
-
-            var server = new TestServer(builder);
-            var client = server.CreateClient();
-
-            var initialResponse = await client.GetAsync("/Apply");
-
-            var antiForgeryCookieValue = ExtractAntiForgeryCookieValueFrom(initialResponse);
-            var antiForgeryToken = ExtractAntiForgeryToken(await initialResponse.Content.ReadAsStringAsync());
+            var initialResponse = await _fixture.Client.GetAsync("/Apply");
+            var antiForgeryValues = await _fixture.ExtractAntiForgeryValues(initialResponse);
 
             var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Apply");
 
-            postRequest.Headers.Add("Cookie", new CookieHeaderValue(_antiForgeryCookieName, antiForgeryCookieValue).ToString());
+            postRequest.Headers.Add("Cookie", new CookieHeaderValue(TestServerFixture.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString());
 
             var formData = new Dictionary<string, string>
             {
-                {_antiForgeryFieldName, antiForgeryToken},
+                {TestServerFixture.AntiForgeryFieldName, antiForgeryValues.fieldValue},
                 {"FirstName", "Sarah"},
                 {"LastName", "Smith"},
                 {"Age", "18"},
@@ -100,35 +48,13 @@ namespace CreditCards.Integration.Test
 
             postRequest.Content = new FormUrlEncodedContent(formData);
 
-            var postReponse = await client.SendAsync(postRequest);
+            var postReponse = await _fixture.Client.SendAsync(postRequest);
 
             postReponse.EnsureSuccessStatusCode();
 
             var responseString = await postReponse.Content.ReadAsStringAsync();
 
             Assert.Contains("Please provide a frequent flyer number", responseString);
-        }
-
-        private static string ExtractAntiForgeryCookieValueFrom(HttpResponseMessage response)
-        {
-            var antiForgeryCookie = response.Headers.GetValues("Set-Cookie").FirstOrDefault(x => x.Contains(_antiForgeryCookieName));
-
-            if (antiForgeryCookie is null)
-                throw new ArgumentException($"Cookie '{_antiForgeryCookieName}' not found in HTTP response", nameof(response));
-
-            var antiForgeryCookieValue = SetCookieHeaderValue.Parse(antiForgeryCookie).Value;
-
-            return antiForgeryCookieValue;
-        }
-
-        private string ExtractAntiForgeryToken(string htmlBody)
-        {
-            var requestVerificationTokenMatch = Regex.Match(htmlBody, $@"\<input name=""{_antiForgeryFieldName}"" type=""hidden"" value=""([^""]+)"" \/\>");
-
-            if (requestVerificationTokenMatch.Success)
-                return requestVerificationTokenMatch.Groups[1].Captures[0].Value;
-
-            throw new ArgumentException($"Anti forgery token '{_antiForgeryFieldName}' not found in HTML", nameof(htmlBody));
         }
     }
 }
---
+++ CreditCards/CreditCards.Integration.Test/TestServerFixture.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CreditCards.Integration.Test
+{
+    public class TestServerFixture : IDisposable
+    {
+        private readonly TestServer _testServer;
+        public static readonly string AntiForgeryFieldName = "__AFTField";
+        public static readonly string AntiForgeryCookieName = "AFTCOokie";
+        public HttpClient Client { get; }
+
+        public TestServerFixture()
+        {
+            // https://github.com/aspnet/Hosting/issues/959#issuecomment-286351703
+            // Fix problem with 500 errors when running the test server
+            var builder = new WebHostBuilder()
+                .UseContentRoot(GetContentRootPath())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+                    ((ConfigurationBuilder)config)
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddEnvironmentVariables();
+                })
+                .UseEnvironment("Development")
+                .UseStartup<Startup>()
+                .ConfigureServices(x =>
+                {
+                    x.AddAntiforgery(t =>
+                    {
+                        t.CookieName = AntiForgeryCookieName;
+                        t.FormFieldName = AntiForgeryFieldName;
+                    });
+                });
+            //.UseApplicationInsights()
+
+            _testServer = new TestServer(builder);
+            Client = _testServer.CreateClient();
+        }
+
+        public async Task<(string fieldValue, string cookieValue)> ExtractAntiForgeryValues(HttpResponseMessage response)
+        {
+            return (ExtractAntiForgeryToken(await response.Content.ReadAsStringAsync()), ExtractAntiForgeryCookieValueFrom(response));
+        }
+
+        private string GetContentRootPath()
+        {
+            var testProjectPath = PlatformServices.Default.Application.ApplicationBasePath;
+            var relativePathToWebProject = @"..\..\..\..\CreditCards";
+            return Path.Combine(testProjectPath, relativePathToWebProject);
+        }
+        
+        private static string ExtractAntiForgeryCookieValueFrom(HttpResponseMessage response)
+        {
+            var antiForgeryCookie = response.Headers.GetValues("Set-Cookie").FirstOrDefault(x => x.Contains(AntiForgeryCookieName));
+
+            if (antiForgeryCookie is null)
+                throw new ArgumentException($"Cookie '{AntiForgeryCookieName}' not found in HTTP response", nameof(response));
+
+            var antiForgeryCookieValue = SetCookieHeaderValue.Parse(antiForgeryCookie).Value;
+
+            return antiForgeryCookieValue;
+        }
+
+        private string ExtractAntiForgeryToken(string htmlBody)
+        {
+            var requestVerificationTokenMatch = Regex.Match(htmlBody, $@"\<input name=""{AntiForgeryFieldName}"" type=""hidden"" value=""([^""]+)"" \/\>");
+
+            if (requestVerificationTokenMatch.Success)
+                return requestVerificationTokenMatch.Groups[1].Captures[0].Value;
+
+            throw new ArgumentException($"Anti forgery token '{AntiForgeryFieldName}' not found in HTML", nameof(htmlBody));
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            _testServer.Dispose();
+        }
+    }
+}
