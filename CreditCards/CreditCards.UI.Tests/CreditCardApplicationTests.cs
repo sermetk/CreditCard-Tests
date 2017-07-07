8fbf23b50bd9193b41ffbfc80e14863863b5652a
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-07 17:55:37 +1000

    Add ui test to validate application details that contains error

--- CreditCards/CreditCards.UI.Tests/CreditCardApplicationTests.cs
+++ CreditCards/CreditCards.UI.Tests/CreditCardApplicationTests.cs
@@ -27,5 +27,23 @@ namespace CreditCards.UI.Tests
 
             Assert.Equal("Credit Card Application - CreditCards", _driver.Title);
         }
+
+        [Fact]
+        public void ShouldValidateApplicationDetails()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:64559/apply");
+
+            _driver.FindElement(By.Name("LastName")).SendKeys("Smith");
+            _driver.FindElement(By.Id("FrequentFlyerNumber")).SendKeys("012345-A");
+            _driver.FindElement(By.Id("Age")).SendKeys("18");
+            _driver.FindElement(By.Id("GrossAnnualIncome")).SendKeys("100000");
+            _driver.FindElement(By.Id("submitApplication")).Click();
+
+            Assert.Equal("Credit Card Application - CreditCards", _driver.Title);
+
+            var firstErrorMessage = _driver.FindElement(By.CssSelector(".validation-summary-errors ul > li"));
+
+            Assert.Equal("Please provide a first name", firstErrorMessage.Text);
+        }
     }
 }
