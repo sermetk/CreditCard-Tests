0353b367fdc9f6fd0957a1fea63a0de39911084a
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-07 14:51:24 +1000

    Allow performing integration test without using EF

--- CreditCards/CreditCards/Startup.cs
+++ CreditCards/CreditCards/Startup.cs
@@ -12,18 +12,28 @@ namespace CreditCards
     public class Startup
     {
         private IConfiguration _configuration;
+        private IHostingEnvironment _environnment;
 
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
             _configuration = configuration;
+            _environnment = environment;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(
-                options => options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            if (_environnment.IsDevelopment())
+            {
+                services.AddDbContext<AppDbContext>(
+                    options => options.UseInMemoryDatabase("TestingDatabase"));
+            }
+            else
+            {
+                services.AddDbContext<AppDbContext>(
+                    options => options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            }
 
             services.AddScoped<ICreditCardApplicationRepository, EntityFrameworkCreditCardApplicationRepository>();
 
@@ -34,9 +44,9 @@ namespace CreditCards
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        public void Configure(IApplicationBuilder app, IServiceProvider serviceProvider)
         {
-            if (env.IsDevelopment())
+            if (_environnment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 //app.UseBrowserLink();
